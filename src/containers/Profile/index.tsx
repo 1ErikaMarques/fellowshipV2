c54e07ce198221728ef8ae06ee35e64882fc3b3d
Erika Marques <1erikagouvea@gmail.com>
2021-08-22 20:34:19 +0200

    adicionando header no profile

--- src/containers/Profile/index.tsx
+++ src/containers/Profile/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { CityImg, RelationshipImg, WorkImg, BirthdayImg, HobbiesImg, PhoneImg } from '../../components/Svgs';
 import theme from '../../styles/theme';
-import { Container } from './styles'
+import { Container, Header, Span } from './styles'
 
 interface ProfileData {
   work?: string;
@@ -10,6 +10,8 @@ interface ProfileData {
   birthday?: string;
   hobbies?: string;
   phone?: string;
+  name: string;
+  photoUrl: string;
 }
 
 interface ProfileProps {
@@ -19,43 +21,55 @@ interface ProfileProps {
 
 
 export function Profile({profileId}: ProfileProps){
-  const  [data, setData] = useState<ProfileData>({relationship: 'alguma coisa'} as ProfileData )
- 
- 
+  const  [data, setData] = useState<ProfileData>({photoUrl:'https://avatars.githubusercontent.com/u/4424108?v=4',name:'Juan Marques'} as ProfileData )
+  const [editButton, setEditButton] = useState('Editar')
+  
+  function handleEditButton(){
+    setEditButton('Salvar')
+  }
+
   return(
      <Container>
+       <Header>
+         <img src={data.photoUrl} alt="foto perfil"></img>
+         <h3>{data.name}</h3>
+         <button onClick={handleEditButton}>{editButton}</button>
+       </Header>
+       
+       
+       <Span>Enfermeiro</Span>
        <WorkImg 
-        fill={data.relationship ?  theme.colors.primary : theme.colors.gray_light} 
-        stroke={data.relationship ?  theme.colors.primary : theme.colors.gray_light}
+        fill={data.relationship ?  theme.colors.primary : theme.colors.gray_dark} 
+        stroke={data.relationship ?  theme.colors.primary : theme.colors.gray_medium}
        />
           
        
        <CityImg 
         fill={data.relationship ?  theme.colors.green : theme.colors.gray_light} 
-        stroke={data.relationship ?  theme.colors.green : theme.colors.gray_light}
+        stroke={data.relationship ?  theme.colors.green : theme.colors.gray_medium}
        />
        
        <RelationshipImg 
         fill={data.relationship ?  theme.colors.pink : theme.colors.gray_light} 
-        stroke={data.relationship ?  theme.colors.pink : theme.colors.gray_light} 
+        stroke={data.relationship ?  theme.colors.pink : theme.colors.gray_medium} 
        />
 
        <BirthdayImg 
-        fill={data.relationship ?  theme.colors.yellow : theme.colors.gray_light} 
-        stroke={data.relationship ?  theme.colors.yellow_light : theme.colors.gray_light} 
+        fill={data.relationship ?  theme.colors.yellow_light : theme.colors.gray_light} 
+        stroke={data.relationship ?  theme.colors.yellow : theme.colors.gray_medium} 
        />
 
        <HobbiesImg        
-        stroke={data.relationship ?  theme.colors.primary : theme.colors.gray_light} 
+        stroke={data.relationship ?  theme.colors.primary : theme.colors.gray_medium} 
        />
 
        <PhoneImg 
         fill={data.relationship ?  theme.colors.red : theme.colors.gray_light} 
-        stroke={data.relationship ?  theme.colors.red : theme.colors.gray_light} 
+        stroke={data.relationship ?  theme.colors.red : theme.colors.gray_medium} 
        />
 
        
-       <span>{data.work}</span>
+       
     </Container>
     
   );   
--- src/containers/Profile/styles.ts
+++ src/containers/Profile/styles.ts
@@ -1,6 +1,63 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  max-width: 585px;
+  max-width: 985px;
+  margin-top: 120px;  
+  background-color: blue; 
   
+  
+`;
+
+export const Header = styled.header`
+  width: 100%;
+  height: auto;
+  display: flex; 
+  
+  align-items: center;
+  
+  background-color: darkred;
+  
+  img{
+    height: 3rem;
+    width: 3rem;
+    border-radius: 30px;    
+    border: none;
+    cursor: pointer;
+      
+  }
+  h3{
+    font-size: 1rem;
+    margin-left: 1rem;
+    font-weight: 400;
+    padding: 0.25rem;
+    background-color: orange;
+    
+  }
+  button{    
+    width: 120px;
+    height: 38px;
+    padding: 0 15px;
+    font-size: 14px;
+    font-weight: 500;
+    letter-spacing: 0.04rem;
+    border-radius: 4px;
+    background: #F4F5F7;
+    color: #666572;
+    border: 1px solid #e9e9e9;
+    cursor: pointer;
+    transition: 200ms ease;
+    
+    
+
+      :hover{
+        color: #7e7e7e;
+        filter: brightness(0.9);
+        transition: 200ms ease;
+      }
+  }
+  
+`;
+
+export const Span = styled.span`
+
 `;
